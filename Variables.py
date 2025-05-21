7b17ea3477951720a4bbfb3530623103b6fb2f22
PhyoThihaOo <phyothihaoo.pto@gmail.com>
2025-05-20 21:01:31 -0400

    Initial Commit

---
+++ Variables.py
@@ -0,0 +1,37 @@
+message = "hello world"
+print(message)
+
+messages = "Hello World!!!"
+print(messages)
+
+message = "Hellllooo Worldddd !!!!!!"
+print(message)
+
+#variables are label that can assign to the values
+
+#1.Strings
+#String is a series of characters. Anything inside quotes is considered a string in Python.
+
+my_string = "This is a string."
+my_string1 = 'This is also a string.'
+
+print('I told my friend,"I am learning Python."')
+print("The language Python is named after 'Monty Python', not the snake.")
+print("One of Python's strength is its diverse and supportive community.")
+
+
+#Methods in String
+
+name = 'Ada Lovelace'
+
+print(name.title()) #capitalizes first letter of each word
+print(name.upper())
+print(name.lower())
+print(name.strip()) #removes whitespace from both ends
+print(name.replace('a','o')) #replaces substring
+print(name.split(' ')) #split string into list
+print(name.isdigit()) #check if all characters are digits
+print(name.isalpha()) #check if all characters are letters
+
+
+
